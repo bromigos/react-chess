4d8ff5a992a7d68e052f87ae6eb187ec8eec24c1
Abel Costilla <aecostilla@gmail.com>
2016-09-05 19:20:30 -0500

    Updated game over logic to include database update patch

--- client/components/ChessboardComponent.js
+++ client/components/ChessboardComponent.js
@@ -54,6 +54,7 @@ export default class ChessBoardComponent extends React.Component{
     else {
       console.log('err -- duplicate position received'); //defensive programming :)
     }
+<<<<<<< 02bcf99e551c67825140e43c4747c79ed352c2f4
 
     // game over check
     if (this.state.chess.game_over()) {
@@ -62,6 +63,8 @@ export default class ChessBoardComponent extends React.Component{
       this.props.showResetGameBtn();
     }
     }
+=======
+>>>>>>> Updated game over logic to include database update patch
   }
 
   updateGameStatus() {
@@ -72,7 +75,9 @@ export default class ChessBoardComponent extends React.Component{
 
     // checkmate?
     if (this.state.chess.in_checkmate() === true) {
+      console.log('game is over: ', this.props.yourGame);
       this.setState({ status: 'Game over, ' + moveColor + ' is in checkmate.' });
+      socket.emit('gameover', this.props.yourGame);
     }
 
     // draw?
